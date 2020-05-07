982db0caaac2053d8118d1c40526f927462cd51a
Primaryheap <s10185319@connect.np.edu.sg>
2020-05-07 17:01:34 +0800

    added video and images res

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        VideoView view = findViewById(R.id.video);
+        view.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.rabbid);
+        view.start();
     }
 }
