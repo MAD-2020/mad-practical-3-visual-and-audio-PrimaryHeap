95ba33fa82de4274680d49427a4803c8729d9053
Primaryheap <s10185319@connect.np.edu.sg>
2020-05-08 11:59:30 +0800

    fixed bugs

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -1,21 +1,29 @@
 package sg.edu.np.mad.week3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.VideoView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        VideoView view = findViewById(R.id.video);
-        view.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.rabbid);
-        view.start();
-    }
+    VideoView view = findViewById(R.id.video);
+
+    view.setVideoPath(
+      "android.resource://" + getPackageName() + "/" + R.raw.rabbid
+    );
+
+    view.start();
+
+    MediaPlayer player = MediaPlayer.create(this, R.raw.baby_laughing);
+
+    player.start();
+
+  }
 }
